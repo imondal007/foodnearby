ceaaf3949ca20e1646720629db68b3c17de4f75f
Imran Mondal <hello@imranmondal.net>
2020-12-21 18:56:37 +0600

    Appropriate loading screen size in mobile device

--- src/components/shimmer/shimmer-rc.tsx
+++ src/components/shimmer/shimmer-rc.tsx
@@ -1,7 +1,7 @@
 import styles from "./styles.module.css";
 
 const Item = () => (
-  <div className="xl:w-1/3 md:w-1/3 w-96 p-4">
+  <div className="xl:w-1/3 md:w-1/3 w-full p-4">
     <div className={styles.animate}>
       <div className="h-52 w-full rounded-md bg-gray-200" />
       <div className="mt-2 h-2 w-full rounded-md bg-gray-200" />
